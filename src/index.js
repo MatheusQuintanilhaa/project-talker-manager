68faa65380b1361e85ef36887bdee6f28b111c4b
Matheus <matheussantos.quintanilha@gmail.com>
2023-04-14 16:32:10 -0300

    req 1 finish

--- src/index.js
+++ src/index.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const fs = require('fs/promises');
 
 const app = express();
 app.use(express.json());
@@ -9,6 +10,21 @@ const PORT = process.env.PORT || '3001';
 // não remova esse endpoint, e para o avaliador funcionar
 app.get('/', (_request, response) => {
   response.status(HTTP_OK_STATUS).send();
+});
+
+const fileReader = async () => {
+  const reader = await fs.readFile('src/talker.json', 'utf-8');
+  return JSON.parse(reader);
+};
+
+const writeFile = async (talkerArray) => {
+  await fs.writeFile('src/talker.json', JSON.stringify(talkerArray));
+};
+
+// Requisito 1
+app.get('/talker', async (req, res) => {
+  const talkers = await fileReader();
+  return res.status(200).json(talkers);
 });
 
 app.listen(PORT, () => {
